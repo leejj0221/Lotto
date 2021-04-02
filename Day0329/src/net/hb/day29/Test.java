5d7fdd8c6209d83617ecc611c0e23471db2816c5
LeeJaeJune <LeeJaeJune@211.184.43.240>
2021-04-02 10:18:53 +0900

    04-02 오전 10:16 이재준

---
+++ Day0329/src/net/hb/day29/Test.java
@@ -0,0 +1,14 @@
+package net.hb.day29;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int i = 0;
+		for(int a=1;a<1;a++) {
+			i=i+1;
+			System.out.println(i + " ");
+			
+		}
+	}//main END
+
+}//Test class END
