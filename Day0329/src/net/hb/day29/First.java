f6cfe480cf7aec104be41f24a1c8b5124fbd0e38
LeeJaeJune <LeeJaeJune@211.184.43.240>
2021-04-02 10:13:19 +0900

    04-02-금요일 자바기초

---
+++ Day0329/src/net/hb/day29/First.java
@@ -0,0 +1,12 @@
+package net.hb.day29;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("First");
+		System.out.println("One");
+		System.out.println("Tea");
+
+	}
+
+}
---
+++ Day0329/src/net/hb/day29/Two.java
@@ -0,0 +1,12 @@
+package net.hb.day29;
+
+public class Two {
+
+	public static void main(String[] args) {
+		System.out.println("Two 9:50");
+		System.out.println("Second");
+		System.out.println("Tea 9:50");
+
+	}
+
+}
